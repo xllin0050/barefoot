7d3f341cf6b1c6ced7bdc9e464b10e5b7ad0ac8d
Lin xinliang <63276356+xllin0050@users.noreply.github.com>
2021-02-16 23:39:23 +0800

    fetch

--- js/index.js
+++ js/index.js
@@ -1,16 +1,55 @@
-let aboutTitle = document.querySelector('.about__title')
-let last_known_scroll_position = 0;
-let ticking = false;
+let productList = document.querySelector('#productList')
 
-function titleTrackingIn() {
-  aboutTitle.classList.add('tracking-in-expand')
-}
+let aboutTitle = document.querySelector('.about__title')
+let weThinkTitle = document.querySelector('.we-think__title')
+let last_known_scroll_position = 0;
 
 window.addEventListener('scroll', function() {
   last_known_scroll_position = window.scrollY;
 
-      if(last_known_scroll_position>250){
-        titleTrackingIn()
-      }
-    })
+    if(last_known_scroll_position>250){
+      aboutTitle.classList.add('tracking-in-expand')
+    }else{
+      aboutTitle.classList.remove('tracking-in-expand')
+    }
 
+    if(last_known_scroll_position>1650){
+      weThinkTitle.classList.add('tracking-in-expand')
+    }else{
+      weThinkTitle.classList.remove('tracking-in-expand')
+    }
+
+})
+
+    
+
+  function getProductInfo(){
+
+    fetch('/mockData/products.json')
+
+      .then(response => response.json())
+
+      .then(
+        function(products){
+
+          productArr = products
+
+          productArr.forEach(el=>{
+            const productType = el.productType
+            const productName = el.productName
+            const productPrice = el.productPrice
+            const productDesc = el.productDesc
+            const productPic = el.productPic
+
+            productList.innerHTML +=`
+            
+            <img src="${productPic}" alt="">
+
+            `
+            console.log(productPrice);
+          })
+        }
+      )
+  }
+
+  getProductInfo()
