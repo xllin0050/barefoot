217ce5af8d13faf9f0d6d55fa293a17632b869cd
Lin xinliang <63276356+xllin0050@users.noreply.github.com>
2021-02-19 00:16:38 +0800

    parallax ok

--- js/index.js
+++ js/index.js
@@ -1,27 +1,49 @@
+const bg =document.querySelector('#bg')
+const stone =document.querySelector('#stone')
+const ele =document.querySelector('#ele')
+
+
 const productList = document.querySelector('#productList')
 const newsList = document.querySelector('#newsList')
 
-let aboutTitle = document.querySelector('.about__title')
-let weThinkTitle = document.querySelector('.we-think__title')
-let last_known_scroll_position = 0;
+
+
+const aboutTitle = document.querySelector('.about__title')
+const weThinkTitle = document.querySelector('.we-think__title')
+let scroll_position = 0;
 
 window.addEventListener('scroll', function() {
-  last_known_scroll_position = window.scrollY;
+  scroll_position = window.scrollY;
 
-    if(last_known_scroll_position>250){
-      aboutTitle.classList.add('tracking-in-expand')
-    }else{
-      aboutTitle.classList.remove('tracking-in-expand')
-    }
+  // banner
+  bg.style.top = scroll_position * 0.5 + 'px';
+  stone.style.top = scroll_position * 2.8 + 'px';
+  grass.style.top = scroll_position * 0.5 + 'px';
+  ele.style.bottom = scroll_position * 0.5 + 'px';
 
-    if(last_known_scroll_position>1650){
-      weThinkTitle.classList.add('tracking-in-expand')
-    }else{
-      weThinkTitle.classList.remove('tracking-in-expand')
-    }
+  if(scroll_position>=210){
+    ele.style.display="none"
+  }else{
+    ele.style.display="unset"
+  }
+
+  // 關於標題
+  if(scroll_position>300){
+    aboutTitle.classList.add('tracking-in-expand')
+  }else{
+    aboutTitle.classList.remove('tracking-in-expand')
+  }
+
+  // 理念標題
+  if(scroll_position>1650){
+    weThinkTitle.classList.add('tracking-in-expand')
+  }else{
+    weThinkTitle.classList.remove('tracking-in-expand')
+  }
 
 })
 
+  // 產品資料
   function getProductInfo(){
 
     fetch('/mockData/products.json')
@@ -46,6 +68,7 @@ window.addEventListener('scroll', function() {
 
   getProductInfo()
 
+  // 消息資料
   function getNewsInfo(){
     
     fetch('/mockData/news.json')
